9960ba95d316701c9dfde0e9bf74cc00d374ad8d
vkathad <v.kathad@brightness-india.com>
2024-01-03 11:44:56 +0530

    changes

--- src/components/WindCave.jsx
+++ src/components/WindCave.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
+import useCheckoutFormContext from '../../../../hook/useCheckoutFormContext';
 
 function WindCave() {
-  
-  return <>ete</>;
+  const { registerPaymentAction } = useCheckoutFormContext();
+  // custom "place order" submit action
+  const paymentSubmitHandler = useCallback(async (values) => {
+    console.log({ values });
+    console.log('yeeeha');
+  }, []);
+
+  // registering custom "place order" action for the payment method
+  useEffect(() => {
+    registerPaymentAction('windcave_pxpay2_iframe', paymentSubmitHandler);
+  }, [registerPaymentAction, paymentSubmitHandler]);
+
+  return <h1>Hello</h1>;
 }
 export default WindCave;
