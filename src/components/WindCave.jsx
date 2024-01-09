aa8945c61d2eabc61c43e90147fc79be268950a7
vkathad <v.kathad@brightness-india.com>
2024-01-09 12:25:37 +0530

    final changes

--- src/components/WindCave.jsx
+++ src/components/WindCave.jsx
@@ -1,38 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { shape, func } from 'prop-types';
-import { get as _get } from 'lodash-es';
 import { paymentMethodShape } from '../../../../utils/payment';
-import useCheckoutFormContext from '../../../../hook/useCheckoutFormContext';
-import usePaymentMethodFormContext from '../../../../components/paymentMethod/hooks/usePaymentMethodFormContext';
-import usePaymentMethodCartContext from '../../../../components/paymentMethod/hooks/usePaymentMethodCartContext';
-
 import RadioInput from '../../../../components/common/Form/RadioInput';
-import useMolliePayment from '../hook/useMolliePayment';
+import usePerformPlaceOrder from '../hooks/usePerformPlaceOrder';
+import usePayOneCheckoutFormContext from '../hooks/usePayOneCheckoutFormContext';
+import Card from '../../../../components/common/Card';
 
 function WindCave({ method, selected, actions }) {
-  const methodCode = method.code;
-  const isSelected = methodCode === selected.code;
-  const { registerPaymentAction } = useCheckoutFormContext();
-  const { placeOrder } = useMolliePayment({ methodCode });
-  const { submitHandler } = usePaymentMethodFormContext();
-  // registering custom "place order" action for the payment method
-  // useEffect(() => {
-  //   registerPaymentAction('windcave_pxpay2_iframe', paymentSubmitHandler);
-  // }, [registerPaymentAction, paymentSubmitHandler]);
-
-  const { methodList } = usePaymentMethodCartContext();
+  const { registerPaymentAction } = usePayOneCheckoutFormContext();
+  const isSelected = method.code === selected.code;
+  const performPlaceOrder = usePerformPlaceOrder(method.code);
+  const placeOrderWithPayPal = useCallback(
+    (values) => performPlaceOrder(values),
+    [performPlaceOrder]
+  );
 
   useEffect(() => {
-    registerPaymentAction(methodCode, placeOrder);
-  }, [registerPaymentAction, methodCode, placeOrder]);
-
-  const handlePaymentMethodSelection = async (event) => {
-    const methodSelected = _get(methodList, `${event.target.value}.code`);
-    await actions.change(event);
-    console.log('yeeeha');
-    console.log('submitcall');
-    await submitHandler(methodSelected);
-  };
+    registerPaymentAction(method.code, placeOrderWithPayPal);
+  }, [method, registerPaymentAction, placeOrderWithPayPal]);
 
   if (!isSelected) {
     return (
@@ -41,7 +26,7 @@ function WindCave({ method, selected, actions }) {
         label={method.title}
         name="paymentMethod"
         checked={isSelected}
-        onChange={handlePaymentMethodSelection}
+        onChange={actions.change}
       />
     );
   }
@@ -53,8 +38,15 @@ function WindCave({ method, selected, actions }) {
           label={method.title}
           name="paymentMethod"
           checked={isSelected}
-          onChange={handlePaymentMethodSelection}
+          onChange={actions.change}
         />
+        <div className="mx-4 my-4">
+          <Card bg="darker">
+            <div className="container flex flex-col justify-center w-4/5">
+              <div>test</div>
+            </div>
+          </Card>
+        </div>
       </div>
     </div>
   );
